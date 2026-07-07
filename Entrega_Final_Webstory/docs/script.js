fb2376dbe673d6468304f7455b58a380c2cb477e
pepacanepa <pepa.canepa@gmail.com>
2026-07-07 11:43:18 -0400

    .

--- Entrega_Final_Webstory/docs/script.js
+++ Entrega_Final_Webstory/docs/script.js
@@ -59,3 +59,18 @@ backToTop.addEventListener("click", () => {
     behavior: "smooth"
   });
 });
+(function(vegaEmbed) {
+    var embedOpt = {"mode": "vega-lite"};
+
+    function showError(el, error){
+        el.innerHTML = ('<div class="error" style="color:red;">'
+                        + '<p>JavaScript Error: ' + error.message + '</p>'
+                        + "<p>Revisa la consola para más detalles.</p>"
+                        + '</div>');
+        throw error;
+    }
+
+    const el = document.getElementById('vis');
+    vegaEmbed("#vis", "visualizaciones/expansion_nacional.html", embedOpt)
+      .catch(error => showError(el, error));
+})(vegaEmbed);
